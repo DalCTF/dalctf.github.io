63b1c0af50c2118e8c91afd9d4b755e5f0414677
Rafael Copstein <rcopstein@gmail.com>
2024-12-26 20:22:36 +0000

    Added new components

---
+++ src/util/colors.ts
@@ -0,0 +1,16 @@
+export type Color = "red" | "green" | "blue" | "orange" | "yellow";
+
+export function hexFor(color: Color): string {
+    switch (color) {
+        case "red":
+            return "#E01A00";
+        case "blue":
+            return "#0090FF";
+        case "green":
+            return "#058F00";
+        case "orange":
+            return "#FF9000";
+        case "yellow":
+            return "#D1B800";
+    }
+}
---
+++ src/util/tags.ts
@@ -0,0 +1,36 @@
+import { hexFor } from "./colors";
+
+export type TagType = "fill" | "border";
+
+export interface Tag {
+    color: string;
+    type: TagType;
+    text: string;
+    url?: string;
+}
+
+const known_tags = new Map<string, Tag>();
+known_tags.set("crypto", { text: "Crypto", color: hexFor("green"), type: "border" });
+known_tags.set("linux", { text: "Linux", color: hexFor("yellow"), type: "border" });
+known_tags.set("misc", { text: "Misc", color: hexFor("orange"), type: "border" });
+known_tags.set("web", { text: "Web", color: hexFor("blue"), type: "border" });
+known_tags.set("pwn", { text: "Pwn", color: hexFor("red"), type: "border" });
+
+export function tagFor(name: string, type: TagType = "border", includeUrl: boolean = true): Tag {
+    var known_tag = known_tags.get(name.toLowerCase());
+
+    if (known_tag) {
+        if (!includeUrl) {
+            delete known_tag.url;
+        }
+
+        known_tag.type = type;
+        return known_tag;
+    }
+
+    return {
+        "text": name,
+        "type": type,
+        "color": "#FFFFFF"
+    }
+}
