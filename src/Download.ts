65766ff7cc54d5b5d04d756568c1080703a2ae96
Rafael Copstein <rcopstein@gmail.com>
2025-01-07 04:37:51 +0000

    Updated download workflow

--- src/Download.ts
+++ src/Download.ts
@@ -1,4 +1,8 @@
+import * as fs from "fs";
 import { Writeups } from "./loaders/Writeups";
 
 const downloaded = await Writeups.shared.load();
-console.log(downloaded.length);
+const updated = downloaded.length;
+
+fs.writeFileSync("updated", `${updated}`);
+console.log("Problems updated:", updated);
